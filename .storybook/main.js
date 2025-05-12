38bc80f5f315aad113bd200bbf5c0a8e252c2cf1
ego <mrego@anakonn.com>
2025-05-12 11:09:15 +0900

    패키지 의존성 업데이트 및 스타일 수정: Storybook 관련 패키지를 8.6.12로 업데이트하고, CSS 파일에서 폰트 패밀리의 따옴표를 일관되게 수정함.

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,7 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    // '@storybook/addon-a11y',
+    '@storybook/addon-a11y',
   ],
   staticDirs: ['../public'],
   framework: {
